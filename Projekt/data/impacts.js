47c69b0abd54b0066d5506b8c16613e80339f6d5
Max Zgonnik <max.zgonnik@uni-ulm.de>
2024-01-31 16:10:28 +0100

    Projekt erstellt + Kreisdiagramm
    Das Kreisdiagramm zu den Einflüssen auf Nautschutzgebieten ist größtenteils fertig. Farben, Schriftarten, -Größen können noch geändert werden

---
+++ Projekt/data/impacts.js
@@ -0,0 +1,51 @@
+let impactData = [
+    {
+        "impactType" : "Stickstoffbelastung",
+        "total" : 3945,
+        "low" : 912,
+        "middle" : 1750,
+        "high" : 1283,
+        "description" : "Eine Stickstoffüberbelastung hat negative Auswirkungen auf die Biodiversität. Pflanzen und Tiere die sich auf die nährstoffarme Lebensbedingungen angepasst haben, werden durch die nährstoffzufuhr des Stickstoffs durch schnell wachsende Pflanzenarten verdrägt, die stickstoffliebend sind. Zudem führt eine Stickstoffüberdüngung in Bäumen zu übermäßigen Wachstum in die Länge, wodurch die Triebe länglich und weich werden. Dadurch wird der Baum anfälliger gegenüber Frost, Hitze, Wind und Krankheiten."
+    },
+    {
+        "impactType" : "Phospor- und Phospatbelastung",
+        "total" : 41,
+        "low" : 1,
+        "middle" : 18,
+        "high" : 22,
+        "description" : "Eine Phosporbelastung in Gewässern führt zu einem massenhaften Vermehrung von Algen, sog. Algenblühten. Algenblühten verbrauchen sehr schnell die verfügbaren Nährstoffe, wie Sauerstoff, wodurch andere Arten, wie Fische massenhaft absterben."
+    },
+    {
+        "impactType" : "Versauerung",
+        "total" : 15,
+        "low" : 3,
+        "middle" : 7,
+        "high" : 5,
+        "description" : "Unter Versauerung versteht man den anstieg des ph-Wertes in Gewässern. Die Versauerung wird durch die steigende Menge an Kohlenstoffdioxid verursacht, die vom Wasser absorbiert wird und in Kohlensäure umgewandelt wird. Durch den erhöhten ph-Wert wird die Biomineralisation von vielen Tierarten (zB: Meeresschnecken) verhindert, wodurch es zu Engpässen in der Nahrungskette & Artenausterben kommt. Zudem wird durch den gestiegenden ph-Wert das Wachstum & die Fortpflanzungsfähigkeit von Fischen beeinträchtigt."
+    },
+    {
+        "impactType" : "Toxische anorganische Chemikalien",
+        "total" : 32,
+        "low" : 21,
+        "middle" : 8,
+        "high" : 3,
+        "description" : "toxische anorganische Chemikalien reichern sie sich in Gewässern & Böden an. Dadurch können diese Chemikalein schnell in die Nahrungskette kommen und in Tieren Krankheiten, Krebs, Hormonstörungen, DNA-Schäden und Geburtsfehler verursachen. Durch die Nahrungskette gelangen diese Chemikalien auch in unsere Nahrung."
+    },
+    {
+        "impactType" : "Toxische organische Chemikalien",
+        "total" : 51,
+        "low" : 17,
+        "middle" : 31,
+        "high" : 3,
+        "description" : "toxische organische Chemikalien zerfallen meistens nicht in harmlose Komponenten, stattdessen reichern sie sich in Gewässern & Böden an. Dadurch können diese Chemikalein schnell in die Nahrungskette kommen und in Tieren Krankheiten, Krebs, Hormonstörungen, DNA-Schäden und Geburtsfehler verursachen. Durch die Nahrungskette gelangen diese Chemikalien auch in unsere Nahrung."
+    },
+    {
+        "impactType" : "Gemischter Einfluss",
+        "total" : 631,
+        "low" : 194,
+        "middle" : 276,
+        "high" : 161,
+        "description" : "Naturschutzgebiete, die von mehrere Einflüssen belastet sind."
+    },
+
+]
